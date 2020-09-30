8cb555d11c04d0a8eae2fa0936f7a494cbba0b77
Jack Byers <jwb421@lehigh.edu>
2020-09-30 18:13:27 -0400

    Renames and file structure

--- code/pkg/chain/Block.go
+++ code/pkg/chain/Block.go
@@ -4,11 +4,12 @@ import (
 	"time"
 )
 
+// Block is the data structure representing a block in the blockchain
 type Block struct {
 	previousHash []byte
 	rootHash     []byte
 	timestamp    time.Time
-	target       [32]byte
+	target       []byte
 	nonce        []byte
 	trie         interface{}
 }
