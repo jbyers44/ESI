cab6ffd73af8d4b7f23d2fe1f45c6fb3dec76dd1
Maxim Vezenov <mav417@lehigh.edu>
2020-09-30 23:58:35 -0400

    finally

--- code/pkg/chain/block.go
+++ code/pkg/chain/block.go
@@ -4,15 +4,14 @@ import (
 	"ESI/pkg/trie"
 	"bytes"
 	"fmt"
-	"time"
 )
 
 // Block is the data structure representing a block in the blockchain
 type Block struct {
 	previousHash []byte
 	rootHash     []byte
-	timestamp    time.Time
-	target       []byte
+	timestamp    int64
+	target       uint32
 	nonce        []byte
 	trie         interface{}
 }
@@ -20,10 +19,10 @@ type Block struct {
 func (block *Block) String(printTrie bool) string {
 	var b bytes.Buffer
 	fmt.Fprintf(&b, "BEGIN HEADER\n")
-	fmt.Fprintf(&b, "[previousHash]      '%s'\n", block.previousHash)
-	fmt.Fprintf(&b, "[rootHash]          '%x'\n", block.rootHash)
-	fmt.Fprintf(&b, "[timestamp]         '%s'\n", block.timestamp)
-	fmt.Fprintf(&b, "[nonce]             '%s'\n", block.nonce)
+	fmt.Fprintf(&b, "[previousHash]      '%b'\n", block.previousHash)
+	fmt.Fprintf(&b, "[rootHash]          '%b'\n", block.rootHash)
+	fmt.Fprintf(&b, "[timestamp]         '%d'\n", block.timestamp)
+	fmt.Fprintf(&b, "[nonce]             '%b'\n", block.nonce)
 	fmt.Fprintf(&b, "END HEADER\n")
 	fmt.Fprintf(&b, "[mpt]\n")
 	if printTrie {
--- code/pkg/chain/chain.go
+++ code/pkg/chain/chain.go
@@ -1,34 +1,70 @@
 package chain
 
 import (
+	"ESI/pkg/crypto"
+	"ESI/pkg/helpers"
 	"ESI/pkg/trie"
 	"crypto/sha256"
 	"time"
 )
 
-var target int = 1 << 31
+var target = uint32(1 << 31)
 
-// Chain represents a pointer to the genesis block in the chain
 type Chain struct {
-	genesis *Block
+	blocks []Block
 }
 
-// NewChain returns a nil-initiated chain
 func NewChain() *Chain {
 	return &Chain{nil}
 }
 
 // Insert a node into the trie
-func (chain *Chain) Insert(trie *trie.MerklePatriciaTrie) {
-	if chain.genesis == nil {
+func (chain *Chain) Insert(previousHash []byte, trie *trie.MerklePatriciaTrie) []byte {
+	if len(chain.blocks) == 0 {
 		h := sha256.New()
 		h.Write(trie.GetRoot().GetHash())
+		hashBytes := h.Sum(nil)
 		timestamp := time.Now().UTC().Unix()
-		chain.genesis = &Block{make([]byte, 0), h.Sum(nil), timestamp, make([]byte, target), []byte{}, trie}
+		chain.blocks = append(chain.blocks, Block{previousHash, hashBytes, timestamp, target, []byte{}, trie})
+		return hashBytes
+	} else {
+		return chain.createBlock(previousHash, trie)
 	}
-	// else {
-	// 	insert(trie.root, value, 0)
-	// }
 }
 
-// func insert(genesis *Block)
+func (chain *Chain) createBlock(previousHash []byte, trie *trie.MerklePatriciaTrie) []byte {
+	h := sha256.New()
+	h.Write(trie.GetRoot().GetHash())
+	hashBytes := h.Sum(nil)
+	timestamp := time.Now().UTC().Unix()
+
+	nonce := crypto.GetNonce(target, hashBytes)
+	newBlock := Block{
+		previousHash,
+		hashBytes,
+		timestamp,
+		target,
+		nonce,
+		trie,
+	}
+	chain.blocks = append(chain.blocks, newBlock)
+	return hashBytes
+}
+
+// InsertBatch takes the not split content of multiple strings and inserts all the blocks, keeping track of the previous hash
+// and constructing the tree for each block
+func (chain *Chain) InsertBatch(rawContent [][]byte) {
+
+	previousHash := make([]byte, 0)
+	for _, values := range rawContent {
+		mpt := trie.NewMerklePatriciaTrie()
+		mpt.InsertBatch(helpers.SplitBytes(values))
+		mpt.GenerateHashes()
+
+		previousHash = chain.Insert(previousHash, mpt)
+	}
+}
+
+func (chain *Chain) GetChain() []Block {
+	return chain.blocks
+}
